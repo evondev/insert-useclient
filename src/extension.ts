28dc1620a6e08f099428c5e3e13a06ce0915a0df
evondev <trananhtuan400@gmail.com>
2023-11-28 15:14:14 +0700

    Update package description and add use server command

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@ import * as vscode from "vscode";
 export function activate(context: vscode.ExtensionContext) {
   const pattern = /^['"]use client['"];?$/m;
 
-  let disposable = vscode.commands.registerCommand(
+  let command1 = vscode.commands.registerCommand(
     "insert-useclient.insertUseClient",
     async () => {
       // insert use client to the top of the current valid javascript file
@@ -35,8 +35,41 @@ export function activate(context: vscode.ExtensionContext) {
       }
     }
   );
+  let command2 = vscode.commands.registerCommand(
+    "insert-useclient.insertUseServer",
+    async () => {
+      // insert use client to the top of the current valid javascript file
+      const editor = vscode.window.activeTextEditor;
 
-  context.subscriptions.push(disposable);
+      if (editor) {
+        const document = editor.document;
+        const text = document.getText();
+        if (pattern.test(text)) {
+          const range = new vscode.Range(
+            new vscode.Position(0, 0),
+            new vscode.Position(1, 0)
+          );
+          editor.edit((editBuilder) => {
+            editBuilder.replace(range, "");
+          });
+          vscode.window.showInformationMessage(
+            "Remove use server successfully!"
+          );
+          return;
+        }
+        const range = new vscode.Range(
+          new vscode.Position(0, 0),
+          new vscode.Position(0, 0)
+        );
+        editor.edit((editBuilder) => {
+          editBuilder.replace(range, "'use server';\n");
+        });
+        vscode.window.showInformationMessage("Insert use server successfully!");
+      }
+    }
+  );
+
+  context.subscriptions.push(command1, command2);
 }
 
 // this method is called when your extension is deactivated
